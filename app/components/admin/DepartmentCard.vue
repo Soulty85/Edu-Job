0c67865a1db7f62c278feffc5c2f5d8e7a0fa786
Soulty85 <ouattarame380@gmail.com>
2025-09-08 06:11:49 -0700

    fix: correction de la coulteur du bouton des departements

--- app/components/admin/DepartmentCard.vue
+++ app/components/admin/DepartmentCard.vue
@@ -1,22 +1,24 @@
 <template>
-  <Card>
-    <CardHeader>
-      <!-- <div class="w-16 h-16 bg-gray-100 flex items-center justify-center rounded">
-        <Icon name="lucide:image" class="w-8 h-8 text-gray-400" />
-      </div> -->
-      <h3 class="font-semibold mt-3">{{ name }}</h3>
-    </CardHeader>
-    <CardContent>
-      <p class="text-sm text-gray-500">
-        {{ offers }} offres actives – {{ candidates }} candidatures en cours
-      </p>
-    </CardContent>
-    <CardFooter>
-      <Button class="w-full bg-black text-white hover:bg-gray-800">
-        Voir les offres
-      </Button>
-    </CardFooter>
-  </Card>
+    <Card>
+        <CardHeader>
+        <!-- <div class="w-16 h-16 bg-gray-100 flex items-center justify-center rounded">
+            <Icon name="lucide:image" class="w-8 h-8 text-gray-400" />
+        </div> -->
+            <h3 class="font-semibold mt-3">{{ name }}</h3>
+        </CardHeader>
+        
+        <CardContent>
+            <p class="text-sm text-gray-500">
+                {{ offers }} offres actives – {{ candidates }} candidatures en cours
+            </p>
+        </CardContent>
+        
+        <CardFooter>
+            <Button class="w-full hover:bg-gray-800">
+                Voir les offres
+            </Button>
+        </CardFooter>
+    </Card>
 </template>
 
 <script setup>
@@ -25,8 +27,8 @@ import { Button } from "@/components/ui/button"
 
 
 defineProps({
-  name: { type: String, required: true },
-  offers: { type: Number, required: true },
-  candidates: { type: Number, required: true },
+    name: { type: String, required: true },
+    offers: { type: Number, required: true },
+    candidates: { type: Number, required: true },
 })
 </script>
