f9ff4274ef78e95d1814fdadda1c514086a555d2
Soulty85 <ouattarame380@gmail.com>
2025-09-17 08:51:31 -0700

    feat(admin): layout de la partie admin

---
+++ app/layouts/admin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="min-h-screen">
+        <Navbar/>
+        <div class=" flex flex-col items-center justify-center">
+        <!-- MAIN -->
+        <main class="container  py-8">
+            <h1 class="text-3xl font-bold text-center">Admin Dashboard</h1>
+            <slot />
+        </main>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Navbar from '~/components/layout/Navbar.vue';
+</script>
--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,59 +1,54 @@
 <template>
-    <div class=" flex flex-col items-center justify-center">
-        <!-- MAIN -->
-        <main class="container  py-8">
-            <h1 class="text-3xl font-bold text-center">Bienvenue Jane</h1>
+    <div class="">
+        <!-- KPIs -->
+        <section class="mt-8">
+            <h2 class="text-3xl font-bold mb-8">KPIs</h2>
             
-            <!-- KPIs -->
-            <section class="mt-8">
-                <h2 class="text-3xl font-bold mb-8">KPIs</h2>
-                
-                <div v-if="!loading" class="grid md:grid-cols-2 gap-4">
-                    <KpiCard
-                        title="Candidatures par poste"
-                        :value="18"
-                        icon="lucide:users"
-                    />
-                    <KpiCard
-                        title="Avancement du recrutement"
-                        :value="72"
-                        unit="%"
-                        icon="lucide:bar-chart-2"
-                        :progress="72"
-                    />
-                </div>
-                <div v-if="loading" class="grid md:grid-cols-2 gap-4">
-                    <KpiSkeleton />
-                    <KpiSkeleton />
-                </div>
-            </section>
+            <div v-if="!loading" class="grid md:grid-cols-2 gap-4">
+                <KpiCard
+                    title="Candidatures par poste"
+                    :value="18"
+                    icon="lucide:users"
+                />
+                <KpiCard
+                    title="Avancement du recrutement"
+                    :value="72"
+                    unit="%"
+                    icon="lucide:bar-chart-2"
+                    :progress="72"
+                />
+            </div>
+            <div v-if="loading" class="grid md:grid-cols-2 gap-4">
+                <KpiSkeleton />
+                <KpiSkeleton />
+            </div>
+        </section>
+        
+        <!-- Départements -->
+        <section class="mt-12">
+            <h2 class="text-3xl font-bold mb-10">Départements</h2>
             
-            <!-- Départements -->
-            <section class="mt-12">
-                <h2 class="text-3xl font-bold mb-10">Départements</h2>
-                
-                <div v-if="!loading" class="grid md:grid-cols-3 gap-4">
-                    <DepartmentCard
-                        
-                        v-for="dept in departments"
-                        :id="dept.id"
-                        :key="dept.id"
-                        :name="dept.name"
-                        :offers="dept.open_positions_count"
-                        :candidates="dept.ongoing_applications_count"
-                    />
-                </div>
-                
-                <div v-if="loading" class="grid md:grid-cols-3 gap-4">
-                    <DepartmentCardSkeleton />
-                    <DepartmentCardSkeleton />
-                    <DepartmentCardSkeleton />
-                    <DepartmentCardSkeleton />
-                    <DepartmentCardSkeleton />
-                    <DepartmentCardSkeleton />
-                </div>
-            </section>
-        </main>
+            <div v-if="!loading" class="grid md:grid-cols-3 gap-4">
+                <DepartmentCard
+                    
+                    v-for="dept in departments"
+                    :id="dept.id"
+                    :key="dept.id"
+                    :name="dept.name"
+                    :offers="dept.open_positions_count"
+                    :candidates="dept.ongoing_applications_count"
+                />
+            </div>
+            
+            <div v-if="loading" class="grid md:grid-cols-3 gap-4">
+                <DepartmentCardSkeleton />
+                <DepartmentCardSkeleton />
+                <DepartmentCardSkeleton />
+                <DepartmentCardSkeleton />
+                <DepartmentCardSkeleton />
+                <DepartmentCardSkeleton />
+            </div>
+        </section>
     </div>
 </template>
 
@@ -64,6 +59,9 @@ import DepartmentCardSkeleton from "~/components/admin/skeleton/DepartmentCardSk
 import KpiSkeleton from "~/components/admin/skeleton/KpiSkeleton.vue";
 
 
+definePageMeta({
+    layout: "admin"
+})
 const config = useRuntimeConfig();
 const loading = ref(true);
 const departments = ref([]);
